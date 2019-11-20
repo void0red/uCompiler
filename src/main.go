81e17b0236727111e860897d06add60620d8fd6a
void0red <void0red@gmail.com>
2019-11-20 16:50:29 +0800

    init

--- src/main.go
+++ src/main.go
@@ -9,15 +9,15 @@ import (
 )
 
 var (
-	input   string
-	output  string
-	treeOut bool
+	input  string
+	output string
+	//treeOut bool
 )
 
 func init() {
 	flag.StringVar(&input, "i", "", "source file to parse")
 	flag.StringVar(&output, "o", "out.png", "output file to save")
-	flag.BoolVar(&treeOut, "t", false, "output ascii tree")
+	//flag.BoolVar(&treeOut, "t", false, "output ascii tree")
 }
 
 func main() {
